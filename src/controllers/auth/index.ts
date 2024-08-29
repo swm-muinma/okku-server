eac244cd2ac03b1baec4b5e4c42e7ac9aa2e54bf
donggni0712 <donggni0712@naver.com>
2024-08-29 13:00:43 +0900

    [IFB-727] feat: 회원탈퇴 router 적용

--- src/controllers/auth/index.ts
+++ src/controllers/auth/index.ts
@@ -3,6 +3,7 @@ import { oauth2loginController } from "./oauth2.controller";
 import { loginController } from "./login.controller";
 import { kakaoLoginWithTokenController } from "./kakaoLoginWithToken.controller";
 import { refreshController } from "./refresh.controller";
+import { withdrawAccountController } from "./withdrawAccount.controller";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.get("/oauth2/code/:code", oauth2loginController);
 router.get("/code/:code", loginController);
 router.post("/app/kakao", kakaoLoginWithTokenController);
 router.post("/refresh", refreshController);
+router.get("/withdraw", withdrawAccountController);
 
 export { router as LoginRouter };
