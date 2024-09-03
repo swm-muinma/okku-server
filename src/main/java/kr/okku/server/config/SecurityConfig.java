7134bf39cdb2b99181a523cc42410c04c3312a77
donggni0712 <donggni0712@naver.com>
2024-09-03 23:31:02 +0900

    [IFB-769] fix: fix cors error with spring security

--- src/main/java/kr/okku/server/config/SecurityConfig.java
+++ src/main/java/kr/okku/server/config/SecurityConfig.java
@@ -1,4 +1,5 @@
 package kr.okku.server.config;
+import jakarta.servlet.http.HttpServletRequest;
 import kr.okku.server.security.JwtAuthenticationFilter;
 import kr.okku.server.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +34,19 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)    // CSRF 보호 비활성화 (필요에 따라 활성화할 수 있습니다)
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource(){
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                        .csrf(AbstractHttpConfigurer::disable)    // CSRF 보호 비활성화 (필요에 따라 활성화할 수 있습니다)
                 .authorizeRequests()
                 .requestMatchers("/oauth2/**","/scrape/**","/reviews/**","/healthy/**").permitAll()  // 공개 경로 설정
                 .anyRequest().authenticated()  // 모든 요청을 인증 요구
@@ -36,4 +55,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+
 }
