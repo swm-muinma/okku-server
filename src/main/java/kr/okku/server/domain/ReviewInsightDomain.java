fca77183479e69288522f1821ba4a1d538d705ce
donggni0712 <donggni0712@naver.com>
2024-10-10 02:40:52 +0900

    [IFB-954] feat: review service 리팩토링

--- src/main/java/kr/okku/server/domain/ReviewInsightDomain.java
+++ src/main/java/kr/okku/server/domain/ReviewInsightDomain.java
@@ -3,17 +3,33 @@ package kr.okku.server.domain;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Collections;
 import java.util.List;
 
 @Data
 @Builder
 public class ReviewInsightDomain {
     private String id;
-    private String platform;
-    private String productPk;
-    private List<ReviewSummaryDomain> cautions;
-    private List<ReviewSummaryDomain> positives;
-    private String consSummary;
-    private String prosSummary;
-    private Integer reviewLen;
+
+    @Builder.Default
+    private String platform = "";
+
+    @Builder.Default
+    private String productPk = "";
+
+    @Builder.Default
+    private List<ReviewSummaryDomain> cautions = Collections.emptyList();
+
+    @Builder.Default
+    private List<ReviewSummaryDomain> positives = Collections.emptyList();
+
+    @Builder.Default
+    private String consSummary = "";
+
+    @Builder.Default
+    private String prosSummary = "";
+
+    @Builder.Default
+    private Integer reviewLen = 0;
 }
+
