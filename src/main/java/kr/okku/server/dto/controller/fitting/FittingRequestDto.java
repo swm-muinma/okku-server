28c67618d6206b0153f9b848c116cc2879cb5179
donggni0712 <donggni0712@naver.com>
2024-10-11 15:46:45 +0900

    [IFB-953] fix: string으로 변경

--- src/main/java/kr/okku/server/dto/controller/fitting/FittingRequestDto.java
+++ src/main/java/kr/okku/server/dto/controller/fitting/FittingRequestDto.java
@@ -7,7 +7,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Data
 public class FittingRequestDto extends BasicRequestDto {
     private MultipartFile image;
-    private boolean isNewImage;
+    private String isNewImage;
     private String imageForUrl;
     private String pickId;
     private String part;
