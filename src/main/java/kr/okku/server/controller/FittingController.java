10a7ce21a23419cedc963b6a0ea5a8afff3bdf92
donggni0712 <donggni0712@naver.com>
2024-10-11 15:07:00 +0900

    [IFB-953] feat: 넣었던 사진 다시 선택할 수 있도록 수정

--- src/main/java/kr/okku/server/controller/FittingController.java
+++ src/main/java/kr/okku/server/controller/FittingController.java
@@ -30,6 +30,6 @@ public class FittingController {
         System.out.println(requestDto);
             var result = fittingService.fitting(userId, requestDto);
             return ResponseEntity.ok(result);
-
     }
+
 }
