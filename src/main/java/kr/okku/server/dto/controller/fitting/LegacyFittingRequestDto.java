bbf9d9946495d8cdcab91ab12d2f2770ff4fc34c
donggni0712 <donggni0712@naver.com>
2024-10-25 11:24:57 +0900

    [IFB-1139] feat: fitting v2 구현

---
+++ src/main/java/kr/okku/server/dto/controller/fitting/LegacyFittingRequestDto.java
@@ -0,0 +1,14 @@
+package kr.okku.server.dto.controller.fitting;
+
+import kr.okku.server.dto.controller.BasicRequestDto;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class LegacyFittingRequestDto extends BasicRequestDto {
+    private MultipartFile image;
+    private String isNewImage;
+    private String imageForUrl;
+    private String pickId;
+    private String part;
+}
