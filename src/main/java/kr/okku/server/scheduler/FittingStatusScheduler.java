dddad7c046adc64c79e78f023633456ab4c979a7
donggni0712 <donggni0712@naver.com>
2024-11-12 12:51:10 +0900

    [IFB-1148] fix: scheduler error 수정

--- src/main/java/kr/okku/server/scheduler/FittingStatusScheduler.java
+++ src/main/java/kr/okku/server/scheduler/FittingStatusScheduler.java
@@ -1,6 +1,7 @@
 package kr.okku.server.scheduler;
 
 import kr.okku.server.adapters.persistence.FittingPersistenceAdapter;
+import kr.okku.server.adapters.persistence.PickPersistenceAdapter;
 import kr.okku.server.domain.FittingDomain;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,20 +16,23 @@ public class FittingStatusScheduler {
 
     private final FittingPersistenceAdapter fittingPersistenceAdapter;
 
-    private Set<String> processingFittingList;
+    private Set<String> processingFittingList = new HashSet<>();
 
     public FittingStatusScheduler(FittingPersistenceAdapter fittingPersistenceAdapter) {
         this.fittingPersistenceAdapter = fittingPersistenceAdapter;
     }
 
-    @Scheduled(cron = "* */15 * * * *")
+    @Scheduled(cron = "0 */15 * * * *")
     public void checkStatus () {
+        System.out.println("call");
         List<FittingDomain> fittingDomains = fittingPersistenceAdapter.findByStatus("waiting");
         fittingDomains.addAll(fittingPersistenceAdapter.findByStatus("processing"));
         Set<String> tempProcessingFittingList = new HashSet<>();
         for (FittingDomain fittingDomain : fittingDomains) {
             if(processingFittingList.contains(fittingDomain.getId())){
-                fittingDomain.setStatus("done");
+                fittingDomain.setStatus("serverError");
+                System.out.println(fittingDomain.getStatus());
+                System.out.println(fittingDomain.getId());
                 fittingPersistenceAdapter.save(fittingDomain);
                 continue;
             }else{
